705a79b9601cffd0ef95d17a39fd8b9b299aadb2
kyle merfeld <kmerfeld1@gmail.com>
2017-07-22 20:14:09 -0500

    adds edit and cleans things up a bit

--- readme.md
+++ readme.md
@@ -1,6 +1,25 @@
-nowdo is a simple application to remind me what im supposed to be working on.
+#nowdo
+nowdo is an todo list that helps you stay on track.
 
-It will read ~/todo.md every ten minutes and notify you what the top item there is.
+I have tried a few different todo lists, and they all had one problem in common. If you don't look at them they are not very useful.
+
+That is why I created nowdo, a todo list that reminds you what you should
+be working on
+
+It will read off the first item in your todo list every 10 minutes and 
+create a notification for you reminding you.
+
+#time
+You can specify a different time by useing the --time flag,
+``` 
+./nowdo -t 30
+```
+The above will cause nowdo to alert you every 30 minutes
+
+#editing todo.md
+You can edit todo.md however you normally would, its just a simple markdown
+file afterall, but you can also use `./nowdo edit` to use your $EDITOR your editor to edit your todo list
+
 
 ~/todo.md should be in the following format
 
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,84 @@
-extern crate notify_rust;
-extern crate regex;
+extern crate notify_rust; extern crate regex;
 extern crate chrono;
-use chrono::Local;
-use std::thread;
-use std::time::Duration;
-use regex::Regex;
+#[macro_use] extern crate clap;
+use clap::{Arg, App, SubCommand};
 use notify_rust::Notification;
-use std::fs::File;
-use std::io::BufReader;
+use std::time::Duration;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::process::Command;
 use std::path::Path;
+use std::thread;
 use std::env;
-
+use chrono::Local;
+use regex::Regex;
 
 fn main() {
+    let matches = App::new("nowdo")
+        .about(crate_description!())
+        .author(crate_authors!("\n"))
+        .version(crate_version!())
+        .arg(Arg::with_name("interval")
+             .short("t")
+             .long("time")
+             .value_name("TIME")
+             .takes_value(true)
+             .help("How long in minutes to wait between notifications"))
+        .subcommand(SubCommand::with_name("edit")
+                    .about("Edit the todo file with $EDITOR"))
+        .get_matches();
 
-    //get path
+    if matches.subcommand_matches("edit").is_some() {
+        edit();
+    }
+    else {
+        let t = matches.value_of("interval").unwrap_or("10");
+        let time: u64 = t.parse().unwrap_or(10);
+        notify(time);
+    }
+}
+
+///Get the home directory
+fn get_path() -> String{
+    //check if file exists
     let mut home = "".to_owned();
     match env::var("HOME") {
         Ok(lang) => home = lang.to_owned(),
-        Err(e) => println!("Couldnt get $HOME env({})", e),
+        Err(e) => {
+            println!("Couldnt get $HOME env({})", e);
+        }
     };
     let home = format!("{}/todo.md", home);
     let path = Path::new(&home);
+    //Create the file if it doesnt exist
+    if !Path::new(path).exists() {
+        println!("todo.md doesnt exist, please run ./nowdo edit to add tasks");
+        File::create(&home).unwrap();
+        std::process::exit(1);
+    }
+    return path.to_str().unwrap().to_owned();
+}
 
+///Open a file in $EDITOR
+fn edit() {
+
+    let file = get_path();
+    let mut editor = "nano".to_owned();
+    if env::var("EDITOR").is_ok() {
+        editor = env::var("EDITOR").unwrap();
+    }
+    Command::new(editor).arg(file).status().unwrap();
+}
+
+///Send a notification every x minutes
+fn notify(time: u64) {
+    //get path
     //If the file already exists
-    if Path::new(path).exists() {
+    let path = get_path();
+    if Path::new(&path).exists() {
         loop {
-            let file = File::open(path).unwrap();
+            let file = File::open(&path).unwrap();
             let mut buf_reader = BufReader::new(file);
             let mut contents = String::new();
             buf_reader.read_to_string(&mut contents).unwrap();
@@ -64,9 +115,9 @@ fn main() {
                 println!("{}\n", date.format("%Y-%m-%d %H:%M:%S"));
 
             }
-            //sleep for ten minutes
-            let ten_minutes = Duration::new(600, 0);
-            thread::sleep(ten_minutes);
+            //sleep for x minutes
+            let wait = Duration::new(time * 60, 0);
+            thread::sleep(wait);
 
         }
     }
@@ -77,9 +128,11 @@ fn main() {
         Notification::new()
             .summary("~/todo.md doesnt exist, creating it")
             .body("You should add some things to the todo file, use the format
-                  #this is a title
-                  then everything below it until a line starts with # is the body")
+                      #this is a title
+                      then everything below it until a line starts with # is the body")
             .show()
             .unwrap();
     }
+
 }
+
